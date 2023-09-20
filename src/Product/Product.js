62a79d4696c39f26370f1e8ec9827290c2763a6f
IraZoloto <9250256135@mail.ru>
2023-09-20 21:54:25 +0300

    install sass and reduct README

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,7 +1,9 @@
+import './Product.scss';
+
 const Product = (props) => {
 
     return (
-        <div>
+        <div className='product'>
             <h2>{props.name}</h2>
             <p className="product-discription">{props.price}</p>
             <button type='submit'>Купить</button>
